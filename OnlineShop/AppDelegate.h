6e88452f08eeb7040a7268e33293742c373eb61f
马 瑞鹏 <maruipp@mamatoMacBook-Pro.local>
2012-03-27 21:08:13 +0800

    Initial Commit

---
+++ OnlineShop/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OnlineShop
+//
+//  Created by 瑞鹏 马 on 12-3-27.
+//  Copyright (c) 2012年 BJTU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OnlineShop/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OnlineShop
+//
+//  Created by 瑞鹏 马 on 12-3-27.
+//  Copyright (c) 2012年 BJTU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
